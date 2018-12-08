5e485ee36f854be7d4a222a092b540ffcd134948
Aguedo Valdes <aguedovp@gmail.com>
2018-12-08 09:25:47 -0600

    refactoring

--- AOC/Season2018/D08/Solution1.cs
+++ AOC/Season2018/D08/Solution1.cs
@@ -1,18 +1,12 @@
-﻿using AOC.Common.Graphs;
-using AOC.Common.Solution;
+﻿using AOC.Common.Solution;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace AOC.Season2018.D08
 {
     class Solution1 : BaseSolution
     {
-        int[] _lines;
-        int _total = 0;
+        private int[] _lines;
+        private int _total = 0;
 
         public override void FindSolution()
         {
@@ -28,14 +22,10 @@ namespace AOC.Season2018.D08
 
             var next = start + 2;
             for (int i = 0; i < child; i++)
-            {
                 next = Read(next);
-            }
 
             for (int i = 0; i < meta; i++)
-            {
                 _total += _lines[next + i];
-            }
 
             return next + meta;
         }
--- AOC/Season2018/D08/Solution2.cs
+++ AOC/Season2018/D08/Solution2.cs
@@ -1,15 +1,12 @@
 ﻿using AOC.Common.Solution;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AOC.Season2018.D08
 {
     class Solution2 : BaseSolution
     {
-        int[] _lines;
+        private int[] _lines;
 
         public override void FindSolution()
         {
@@ -24,42 +21,31 @@ namespace AOC.Season2018.D08
             var meta = _lines[start + 1];
 
             var next = start + 2;
-            var item = new Item { Next = start + 2 };
-
             var result = 0;
             if (child == 0)
             {
                 for (int i = 0; i < meta; i++)
-                {
-                    result += _lines[item.Next + i];
-                }
+                    result += _lines[next + i];
             }
             else
             {
-                var dict = new Dictionary<int, int>();            
-
+                var dict = new Dictionary<int, int>();   
                 for (int i = 0; i < child; i++)
                 {
-                    var temp = Read(item.Next);
-                    item.Next = temp.next;
+                    var temp = Read(next);
+                    next = temp.next;
                     dict[i + 1] = temp.value;
                 }
 
                 for (int i = 0; i < meta; i++)
                 {
-                    var temp = _lines[item.Next + i];
+                    var temp = _lines[next + i];
                     if (dict.ContainsKey(temp))
                         result += dict[temp];
                 }
             }
 
-            return (item.Next + meta, result);
-        }
-
-        class Item
-        {
-            public int Next { get; set; }
-            public int Value { get; set; }
+            return (next + meta, result);
         }
     }
 }
